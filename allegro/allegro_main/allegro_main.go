cc4a382f33ed45ffd4cc79d107ebb8c4d5d1490c
Damien Radtke <damienradtke@gmail.com>
2013-11-12 23:47:58 -0600

    Add main module.

---
+++ allegro/allegro_main/allegro_main.go
@@ -0,0 +1,12 @@
+package allegro_main
+
+/*
+#cgo pkg-config: allegro_main-5.0
+#include <allegro5/allegro.h>
+*/
+import "C"
+
+// This method does nothing, but it forces Go
+// to include this module in the build
+func Init() {
+}
