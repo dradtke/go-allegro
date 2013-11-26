cd849bc0cae321ee04f3936ab8dfa84cdb9f39af
Damien Radtke <admin@damienradtke.org>
2013-11-26 13:05:50 -0600

    Added dialog doc header.

--- allegro/dialog/dialog.go
+++ allegro/dialog/dialog.go
@@ -1,15 +1,17 @@
+// Package dialog provides support for Allegro's native dialog module.
 package dialog
 
 /*
 #cgo pkg-config: allegro_dialog-5.0
 #include <allegro5/allegro.h>
+#include <stdio.h>
 #include <allegro5/allegro_native_dialog.h>
 
-void _al_free_string(char *data) {
+void free_string(char *data) {
 	al_free(data);
 }
 
-void _al_append_native_text_log(ALLEGRO_TEXTLOG *log, char *str) {
+void append_to_log(ALLEGRO_TEXTLOG *log, char *str) {
 	al_append_native_text_log(log, str);
 }
 */
@@ -70,9 +72,9 @@ func CreateNativeFileDialog(initial_path, title, patterns string, flags FileChoo
 	initial_path_ := C.CString(initial_path)
 	title_ := C.CString(title)
 	patterns_ := C.CString(patterns)
-	defer C._al_free_string(initial_path_)
-	defer C._al_free_string(title_)
-	defer C._al_free_string(patterns_)
+	defer C.free_string(initial_path_)
+	defer C.free_string(title_)
+	defer C.free_string(patterns_)
 	d := C.al_create_native_file_dialog(initial_path_, title_, patterns_, C.int(flags))
 	if d == nil {
 		return nil, errors.New("failed to create native file chooser dialog")
@@ -95,9 +97,9 @@ func ShowNativeMessageBox(display *allegro.Display, title, heading, text string,
 	title_ := C.CString(title)
 	heading_ := C.CString(heading)
 	text_ := C.CString(text)
-	defer C._al_free_string(title_)
-	defer C._al_free_string(heading_)
-	defer C._al_free_string(text_)
+	defer C.free_string(title_)
+	defer C.free_string(heading_)
+	defer C.free_string(text_)
 	res := C.al_show_native_message_box((*C.ALLEGRO_DISPLAY)(unsafe.Pointer(display)),
 		title_, heading_, text_, nil, C.int(flags))
 	return MessageBoxResult(res)
@@ -108,10 +110,10 @@ func ShowNativeMessageBoxWithButtons(display *allegro.Display, title, heading, t
 	heading_ := C.CString(heading)
 	text_ := C.CString(text)
 	buttons_ := C.CString(strings.Join(buttons, "|"))
-	defer C._al_free_string(title_)
-	defer C._al_free_string(heading_)
-	defer C._al_free_string(text_)
-	defer C._al_free_string(buttons_)
+	defer C.free_string(title_)
+	defer C.free_string(heading_)
+	defer C.free_string(text_)
+	defer C.free_string(buttons_)
 	res := int(C.al_show_native_message_box((*C.ALLEGRO_DISPLAY)(unsafe.Pointer(display)),
 		title_, heading_, text_, buttons_, C.int(flags))) - 1
 
@@ -124,7 +126,7 @@ func ShowNativeMessageBoxWithButtons(display *allegro.Display, title, heading, t
 
 func OpenNativeTextLog(title string, flags TextLogFlags) (*TextLog, error) {
 	title_ := C.CString(title)
-	defer C._al_free_string(title_)
+	defer C.free_string(title_)
 	l := C.al_open_native_text_log(title_, C.int(flags))
 	if l == nil {
 		return nil, errors.New("failed to open native text log")
@@ -156,14 +158,23 @@ func (log *TextLog) Close() {
 func (log *TextLog) Append(format string, a ...interface{}) {
 	var text string
 	if len(a) == 0 {
-		text = format + "\n"
+		text = format
 	} else {
-		text = fmt.Sprintf(format + "\n", a)
+		text = fmt.Sprintf(format, a)
 	}
 	text_ := C.CString(text)
-	defer C._al_free_string(text_)
+	defer C.free_string(text_)
 	// C.al_append_native_text_log()
-	C._al_append_native_text_log((*C.ALLEGRO_TEXTLOG)(log), text_)
+	C.append_to_log((*C.ALLEGRO_TEXTLOG)(log), text_)
+}
+
+func (log *TextLog) Appendln(format string, a ...interface{}) {
+	text := format + "\n"
+	if len(a) == 0 {
+		log.Append(text)
+	} else {
+		log.Append(text, a)
+	}
 }
 
 func (log *TextLog) EventSource() (*allegro.EventSource) {
