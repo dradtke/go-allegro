ba7788b7f9be115c8d69a4107697012125e440f5
Damien Radtke <damienradtke@gmail.com>
2013-12-16 13:07:43 -0600

    Added Allegro file options for loading fonts.

--- allegro/font/ttf/ttf.go
+++ allegro/font/ttf/ttf.go
@@ -11,8 +11,10 @@ void _al_free_string(char *data) {
 */
 import "C"
 import (
+	"errors"
 	"fmt"
 	"github.com/dradtke/go-allegro/allegro/font"
+	"github.com/dradtke/go-allegro/allegro"
 	"unsafe"
 )
 
@@ -46,12 +48,34 @@ func LoadFont(filename string, size int, flags TtfFlags) (*font.Font, error) {
 	return (*font.Font)(unsafe.Pointer(f)), nil
 }
 
+func LoadFontF(file *allegro.File, filename string, size int, flags TtfFlags) (*font.Font, error) {
+	filename_ := C.CString(filename)
+	defer C._al_free_string(filename_)
+	f := C.al_load_ttf_font_f((*C.ALLEGRO_FILE)(unsafe.Pointer(file)), filename_,
+		C.int(size), C.int(flags))
+	if f == nil {
+		return nil, errors.New("failed to load font from file")
+	}
+	return (*font.Font)(unsafe.Pointer(f)), nil
+}
+
 func LoadFontStretch(filename string, w, h int, flags TtfFlags) (*font.Font, error) {
 	filename_ := C.CString(filename)
 	defer C._al_free_string(filename_)
 	f := C.al_load_ttf_font_stretch(filename_, C.int(w), C.int(h), C.int(flags))
 	if f == nil {
 		return nil, fmt.Errorf("failed to load ttf font at '%s'", filename)
+	}
+	return (*font.Font)(unsafe.Pointer(f)), nil
+}
+
+func LoadFontStretchF(file *allegro.File, filename string, w, h int, flags TtfFlags) (*font.Font, error) {
+	filename_ := C.CString(filename)
+	defer C._al_free_string(filename_)
+	f := C.al_load_ttf_font_stretch_f((*C.ALLEGRO_FILE)(unsafe.Pointer(file)),
+		filename_, C.int(w), C.int(h), C.int(flags))
+	if f == nil {
+		return nil, errors.New("failed to load font from file")
 	}
 	return (*font.Font)(unsafe.Pointer(f)), nil
 }
