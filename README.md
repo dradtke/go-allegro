80ad5179a8804c570ab0d0f4a20258f7b7514561
Damien Radtke <damienradtke@gmail.com>
2013-12-19 14:53:16 -0600

    Turned the provided example into a technically functional game.

--- README.md
+++ README.md
@@ -5,9 +5,9 @@ This repository contains bindings for writing [Allegro 5](http://alleg.sourcefor
 
 I assume that you have a working Allegro 5 development environment set up. If not, go do that first.
 
-Screenshot of one of the included examples (open the `example` folder for more details on the included examples):
+Here's an example of the binding in action:
 
-![screenshot](https://github.com/dradtke/go-allegro/raw/5.0/example/img/screenshot.png)
+![screenshot](https://github.com/dradtke/go-allegro/raw/5.0/example/screenshot.png)
 
 Function documentation is included in the source, but it's pulled directly from Allegro's C API documentation, so not everything will line up as far as parameters and return values. However, the C API maps pretty well to the Go API, so if you're familiar with the patterns (e.g. `error`'s instead of boolean success values, multiple return values instead of output parameters, object functions as instance methods on structs), then it shouldn't be hard to figure out what's going on.
 
--- example/hello.go
+++ example/hello.go
@@ -4,29 +4,128 @@ import (
 	"github.com/dradtke/go-allegro/allegro"
 	"github.com/dradtke/go-allegro/allegro/color"
 	"github.com/dradtke/go-allegro/allegro/font"
-	//"github.com/dradtke/go-allegro/allegro/image"
+	"github.com/dradtke/go-allegro/allegro/image"
+	"github.com/dradtke/go-allegro/allegro/primitives"
 	"log"
+	"math"
 )
 
-func main() {
-	var (
-		display     *allegro.Display
-		eventQueue  *allegro.EventQueue
-		builtinFont *font.Font
-		//gordon      *allegro.Bitmap
-		textColor   allegro.Color
-		running     bool = true
-		score       int  = 0
-		err         error
-	)
+var (
+	display    *allegro.Display
+	eventQueue *allegro.EventQueue
+	timer      *allegro.Timer
 
+	gordon      *allegro.Bitmap
+	builtinFont *font.Font
+	floralWhite allegro.Color
+	black       allegro.Color
+	green       allegro.Color
+	yellow      allegro.Color
+	red         allegro.Color
+
+	running  bool = true
+	playing  bool = true
+	gameWon  bool
+	clicks   int
+	timeLeft int = 60
+	err      error
+
+	centerX float32
+	centerY float32
+)
+
+const (
+	SCORE_TIME = 0.5
+)
+
+// Handy method that loads a known color, quitting the program if it fails.
+func loadColor(name color.Name) allegro.Color {
+	var r, g, b float32
+	if r, g, b, err = color.NameToRgb(name); err != nil {
+		log.Fatal(err)
+	}
+	return allegro.MapRGBf(r, g, b)
+}
+
+func loadAssets() {
+	// colors
+	floralWhite = loadColor(color.FLORAL_WHITE)
+	green = loadColor(color.GREEN)
+	yellow = loadColor(color.YELLOW)
+	red = loadColor(color.RED)
+	black = allegro.MapRGB(0, 0, 0)
+
+	// fonts
+	font.Init()
+	if builtinFont, err = font.Builtin(); err != nil {
+		log.Fatal(err)
+	}
+
+	// images
+	image.Init()
+	if gordon, err = allegro.LoadBitmap("img/gopher.png"); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func render() {
+	allegro.ClearToColor(black)
+
+	if playing {
+		gordon.Draw(centerX-float32(gordon.Width()/2), centerY-float32(gordon.Height()/2), 0)
+
+		font.DrawTextf(builtinFont, floralWhite, centerX, centerY-100, font.ALIGN_CENTRE, "Clicks: %3d", clicks)
+		font.DrawText(builtinFont, floralWhite, centerX, centerY+100, font.ALIGN_CENTRE,
+			"Can you reach 100 clicks before the time runs out?!")
+
+		start_theta := float32(-math.Pi / 2)
+		delta_theta := -2 * math.Pi * (float32(timeLeft) / 60)
+
+		var pieColor allegro.Color
+		switch {
+		case timeLeft < 15:
+			pieColor = red
+		case timeLeft < 30:
+			pieColor = yellow
+		default:
+			pieColor = green
+		}
+
+		primitives.DrawFilledPieslice(primitives.Point{centerX, 60}, 30, start_theta, delta_theta, pieColor)
+	} else {
+		if gameWon {
+			font.DrawText(builtinFont, floralWhite, centerX, centerY, font.ALIGN_CENTRE,
+				"Congratulations!")
+		} else {
+			font.DrawText(builtinFont, floralWhite, centerX, centerY, font.ALIGN_CENTRE,
+				"Better luck next time. :-(")
+		}
+	}
+
+	allegro.FlipDisplay()
+}
+
+func endGame(won bool) {
+	playing = false
+	gameWon = won
+}
+
+func main() {
 	allegro.SetNewDisplayFlags(allegro.WINDOWED)
 	if display, err = allegro.CreateDisplay(640, 480); err != nil {
 		log.Fatal(err)
 	} else {
 		defer display.Destroy()
 		display.SetWindowTitle("Hello, Go!")
+		centerX = float32(display.Width() / 2)
+		centerY = float32(display.Height() / 2)
 	}
+
+	if err = allegro.InstallMouse(); err != nil {
+		log.Fatal(err)
+	}
+
+	loadAssets()
 
 	if eventQueue, err = allegro.CreateEventQueue(); err != nil {
 		log.Fatal(err)
@@ -34,27 +133,21 @@ func main() {
 		defer eventQueue.Destroy()
 	}
 
-	/*
-	image.Init()
-	if gordon, err = allegro.LoadBitmap("img/gordon-the-gopher.png"); err != nil {
-		log.Fatal(err)
-	}
-	*/
-
-	font.Init()
-	if builtinFont, err = font.Builtin(); err != nil {
+	if timer, err = allegro.CreateTimer(SCORE_TIME); err != nil {
 		log.Fatal(err)
 	}
 
-	textR, textG, textB, err := color.NameToRgb(color.FLORAL_WHITE)
-	if err != nil {
+	var mouseEventSource *allegro.EventSource
+	if mouseEventSource, err = allegro.MouseEventSource(); err != nil {
 		log.Fatal(err)
 	}
-	textColor = allegro.MapRGB(byte(textR), byte(textG), byte(textB))
-
 	eventQueue.RegisterEventSource(display.EventSource())
-	allegro.ClearToColor(allegro.MapRGB(0, 0, 0))
-	allegro.FlipDisplay()
+	eventQueue.RegisterEventSource(timer.EventSource())
+	eventQueue.RegisterEventSource(mouseEventSource)
+
+	render()
+	redraw := false
+	timer.Start()
 
 	for {
 		event, found := eventQueue.WaitForEventUntil(allegro.NewTimeout(0.06))
@@ -63,6 +156,22 @@ func main() {
 			case allegro.DisplayCloseEvent:
 				running = false
 				break
+			case allegro.TimerEvent:
+				if playing {
+					timeLeft--
+					redraw = true
+					if timeLeft == 0 {
+						endGame(false)
+					}
+				}
+			case allegro.MouseButtonDownEvent:
+				if playing {
+					clicks++
+					redraw = true
+					if clicks == 100 {
+						endGame(true)
+					}
+				}
 			}
 		}
 
@@ -70,9 +179,9 @@ func main() {
 			break
 		}
 
-		allegro.ClearToColor(allegro.MapRGB(0, 0, 0))
-		//gordon.Draw(float32((display.Width()-gordon.Width())/2), float32((display.Height()-gordon.Height())/2), 0)
-		font.DrawTextf(builtinFont, textColor, 50, 50, font.ALIGN_LEFT, "Score: %d", score)
-		allegro.FlipDisplay()
+		if redraw && eventQueue.IsEmpty() {
+			render()
+			redraw = false
+		}
 	}
 }
