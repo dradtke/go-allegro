5d20e73226a2368f93525e80c73fa0b2ba08015c
Damien Radtke <dradtke@channeliq.com>
2014-05-05 11:09:42 -0500

    Updating example formatting.

--- allegro/system.go
+++ allegro/system.go
@@ -9,73 +9,73 @@ C-specific, but otherwise should still be useful.
 
 A bare-bones program might look something like this:
 
-	package main
+    package main
 
-	import (
-		"github.com/dradtke/go-allegro/allegro"
-	)
+    import (
+    	"github.com/dradtke/go-allegro/allegro"
+    )
 
-	const FPS int = 60
+    const FPS int = 60
 
-	func main() {
+    func main() {
         if err := allegro.Install(); err != nil {
-	        panic(err)
-	    }
-	    defer allegro.Uninstall()
+            panic(err)
+        }
+        defer allegro.Uninstall()
 
-	    var (
-	        display    *allegro.Display
-	        eventQueue *allegro.EventQueue
-	        running    bool = true
-	        err        error
-	    )
+        var (
+            display    *allegro.Display
+            eventQueue *allegro.EventQueue
+            running    bool = true
+            err        error
+        )
 
-	    // Create a 640x480 window and give it a title.
-	    allegro.SetNewDisplayFlags(allegro.WINDOWED)
-	    if display, err = allegro.CreateDisplay(640, 480); err == nil {
-	        defer display.Destroy()
-	        display.SetWindowTitle("Hello World")
-	    } else {
-	        panic(err)
-	    }
+        // Create a 640x480 window and give it a title.
+        allegro.SetNewDisplayFlags(allegro.WINDOWED)
+        if display, err = allegro.CreateDisplay(640, 480); err == nil {
+            defer display.Destroy()
+            display.SetWindowTitle("Hello World")
+        } else {
+            panic(err)
+        }
 
-	    // Create an event queue. All of the event sources we care about should
-	    // register themselves to this queue.
-	    if eventQueue, err = allegro.CreateEventQueue(); err == nil {
-	        defer eventQueue.Destroy()
-	    } else {
-	        panic(err)
-	    }
+        // Create an event queue. All of the event sources we care about should
+        // register themselves to this queue.
+        if eventQueue, err = allegro.CreateEventQueue(); err == nil {
+            defer eventQueue.Destroy()
+        } else {
+            panic(err)
+        }
 
-	    // Calculate the timeout value based on the desired FPS.
-	    timeout := float64(1) / float64(FPS)
+        // Calculate the timeout value based on the desired FPS.
+        timeout := float64(1) / float64(FPS)
 
-	    // Register event sources.
-	    eventQueue.RegisterEventSource(display.EventSource())
+        // Register event sources.
+        eventQueue.RegisterEventSource(display.EventSource())
 
-	    // Set the screen to black.
-	    allegro.ClearToColor(allegro.MapRGB(0, 0, 0))
-	    allegro.FlipDisplay()
+        // Set the screen to black.
+        allegro.ClearToColor(allegro.MapRGB(0, 0, 0))
+        allegro.FlipDisplay()
 
-	    // Main loop.
-	    var event allegro.Event
-	    for {
-	        found := eventQueue.WaitForEventUntil(allegro.NewTimeout(timeout), &event)
-	        if found {
-	            switch event.Type {
-	            case allegro.EVENT_DISPLAY_CLOSE:
-	                running = false
-	                break
+        // Main loop.
+        var event allegro.Event
+        for {
+            found := eventQueue.WaitForEventUntil(allegro.NewTimeout(timeout), &event)
+            if found {
+                switch event.Type {
+                case allegro.EVENT_DISPLAY_CLOSE:
+                    running = false
+                    break
 
-	                // Handle other events here.
-	            }
-	        }
+                    // Handle other events here.
+                }
+            }
 
-	        if !running {
-	            return
-	        }
-	    }
-	}
+            if !running {
+                return
+            }
+        }
+    }
 */
 package allegro
 
